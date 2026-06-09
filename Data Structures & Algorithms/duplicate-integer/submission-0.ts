40f3031864413faf562c78adc9ba999d70383e94
Ankur Joshi <joshi.ankur.cz@gmail.com>
2026-06-10 01:27:40 +0200

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,58 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        /** Brute Force 
+         * Time & Space Complexity
+            Time complexity: O(n^2)
+            Space complexity: O(1)
+        */
+        // for (let i = 0; i < nums.length; i++) {
+        //     for (let j = i + 1; j < nums.length; j++) {
+        //         if (nums[j] === nums[i]) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        /** Using Sorting 
+         * Time & Space Complexity
+            Time complexity: O(n log n)
+            Space complexity: O(1) or O(n) depending on sorting algorithm
+        */
+        // nums.sort((a, b) => a - b);
+        // for (let i = 1; i < nums.length; i++) {
+        //     if (nums[i] === nums[i - 1]) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        /** Using Hash Set - Using a hash set allows constant-time lookups, 
+         * making this approach much more efficient than comparing every pair.
+         * * Time & Space Complexity
+            Time complexity: O(n)
+            Space complexity: O(n)
+         */
+        // const seen = new Set();
+        // for (const num of nums) {
+        //     if (seen.has(num)) {
+        //         return true;
+        //     }
+        //     seen.add(num);
+        // }
+        // return false;
+
+        /** Using Hash Set Length
+         * simply compare the length of the set to the length of the original array.
+            If duplicates exist, the set will contain fewer elements
+         * Time & Space Complexity
+            Time complexity: O(n)
+            Space complexity: O(n)
+         */
+         return new Set(nums).size < nums.length;
+    }
+}
