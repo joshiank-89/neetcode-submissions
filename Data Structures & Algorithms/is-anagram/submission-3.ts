d89a569901f8de41c2a364d9b8942b108b011e86
Ankur Joshi <joshi.ankur.cz@gmail.com>
2026-06-10 02:08:55 +0200

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.ts
@@ -0,0 +1,73 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+    */
+    isAnagram(s: string, t: string): boolean {
+        /** Using Sorting
+        * Strings lack a .sort() method: In TypeScript, 
+        * strings are immutable. To sort them, you must 
+        * first convert them into an array using .split(''), 
+        * sort the array, and then turn it back into a string 
+        * using .join('').
+        * Time & Space Complexity
+        * Time complexity: O(n log n + m log m)
+        * Space Complexity: O(1) or O(n+m) depending on the sorting algorithm
+        * where n is the length of string s and m is the length of string t.
+        */
+        // if (s.length !== t.length) {
+        //     return false;
+        // }
+        
+        // const sortedS = s.split('').sort().join('');
+        // const sortedT = t.split('').sort().join('');
+
+        // if (sortedS != sortedT) {
+        //     return false;
+        // }
+        // return true;
+
+        /** Using Hash Map 
+        * Time & Space Complexity
+        * Time complexity: O(n + m)
+        * Space Complexity: O(1) since we have at most 26 different characters
+        * where n is the length of string s and m is the length of string t.
+        */
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const count: Record<string, number> = {};
+
+        for (let i = 0; i < s.length; i++) {
+            count[s[i]] = (count[s[i]] || 0) + 1;
+            count[t[i]] = (count[t[i]] || 0) - 1;
+        }
+
+        for (const char in count) {
+            if (count[char] !== 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+        /** Using Hash Table (Array) 
+        * Time & Space Complexity
+        * Time complexity: O(n + m)
+        * Space Complexity: O(1) since we have at most 26 different characters
+        * where n is the length of string s and m is the length of string t.
+        */
+        // if (s.length !== t.length) {
+        //     return false;
+        // }
+
+        // const count = new Array(26).fill(0);
+        // for (let i = 0; i < s.length; i++) {
+        //     count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        //     count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+        // }
+        // return count.every((val) => val === 0);
+    }
+}
